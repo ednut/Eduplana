2dd9dadc750aacd0123096125150f26f4219f92b
Babatunde Adegbite <babatundea@wakanow.com>
2017-08-23 12:06:58 +0100

    fix navigation active state

--- assets/js/controllers/generalController.js
+++ assets/js/controllers/generalController.js
@@ -3,8 +3,8 @@ var app = angular.module('myApp.controllers');
 
 app.controller('generalController', function($scope, $location) {
 	
-    $scope.getClass = function (path) {
-	  return ($location.path().substr(0, path.length) === path) ? 'current' : '';
-	}
-
+	$scope.isActive = function (viewLocation) { 
+        return viewLocation === $location.path();
+    };
+    console.log($location.path())
 });
