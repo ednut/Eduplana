152b0f710f31fcfbaff17a04386ef4b34552a551
Babatunde Adegbite <babatundea@wakanow.com>
2017-08-23 10:44:19 +0100

    made the app angular

---
+++ assets/js/app.js
@@ -0,0 +1,48 @@
+	angular.module('myApp.controllers', []);
+	angular.module('myApp.models', []);
+	var myApp = angular.module('myApp', ['ui.router', 'myApp.controllers', 'myApp.models']);
+
+	// configure our routes
+	myApp.config(function($stateProvider, $urlRouterProvider) {
+	  //
+	  // For any unmatched url, redirect to /state1
+	  $urlRouterProvider.otherwise("/");
+	  //
+	  // Now set up the states
+	  $stateProvider
+	    .state('home', {
+	      url: "/",
+	      templateUrl: "partials/home.html"
+	    })
+
+	    .state('about', {
+	      url: "/about",
+	      templateUrl: "partials/about.html"
+	    })
+
+	    .state('data', {
+	      url: "/data",
+	      templateUrl: "partials/data.html"
+	    })
+
+	    .state('infographics', {
+	      url: "/infographics",
+	      templateUrl: "partials/infographics.html"
+	    })
+
+	    .state('contact', {
+	      url: "/contact",
+	      templateUrl: "partials/contact.html"
+	    })
+
+	    .state('blog', {
+	      url: "/blog",
+	      templateUrl: "partials/blog.html"
+	    })
+
+	    .state('blogDetails', {
+	      url: "/blog",
+	      templateUrl: "partials/blogDetails.html"
+	    })
+
+	});
---
+++ assets/js/controllers/generalController.js
@@ -0,0 +1,8 @@
+'use strict';
+var app = angular.module('myApp.controllers');
+
+app.controller('generalController', function($scope) {
+	
+    $scope.message = 'Hello World';
+
+});
