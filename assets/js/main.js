32b51488fb2ad037e68d6713a8074d9a06b43891
Babatunde Adegbite <babatundea@wakanow.com>
2017-09-11 21:54:19 +0100

    new update

--- assets/js/main.js
+++ assets/js/main.js
@@ -130,17 +130,3 @@ $(window).load(function(){
     });
 
 });
-
-
-
-$(window).load(function() {
-
-	$("a.popup").fancybox({
-		'transitionIn'	:	'elastic',
-		'transitionOut'	:	'elastic',
-		'speedIn'		:	600, 
-		'speedOut'		:	200, 
-		'overlayShow'	:	false
-	});
-	
-});
